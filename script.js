8614eed5621a85776da35107bcc0b374550ada02
aus <wasabideeznuts@gmail.com>
2026-02-08 15:02:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,24 +20,6 @@ envelope.addEventListener("click", () => {
     },50);
 });
 
-// Logic to move the NO btn
-
-//noBtn.addEventListener("mouseover", () => {
-    //const min = 200;
-    //const max = 200;
-
-    //const distance = Math.random() * (max - min) + min;
-    //const angle = Math.random() * Math.PI * 2;
-
-    //const moveX = Math.cos(angle) * distance;
-    //const moveY = Math.sin(angle) * distance;
-
-    //noBtn.style.transition = "transform 0.3s ease";
-    //noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
-//});
-
-// Logic to make YES btn to grow
-
     let yesScale = 1;
 
  yesBtn.style.position = "relative"
@@ -60,7 +42,7 @@ envelope.addEventListener("click", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "WOOOOOO!";
 
     catImg.src = "cat_dance.gif";
 
@@ -69,4 +51,5 @@ yesBtn.addEventListener("click", () => {
     buttons.style.display = "none";
 
     finalText.style.display = "block";
+
 });
